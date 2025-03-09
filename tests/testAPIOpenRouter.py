a142e8c96a8fb9d5794442d5c15750b552b35dc3
Nicocoooooo <nicolasantoine2003@gmail.com>
2025-03-09 13:19:41 +0100

    fix : Modifications pour clé API nom du .env.

--- tests/testAPIOpenRouter.py
+++ tests/testAPIOpenRouter.py
@@ -2,12 +2,11 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 
-# Charger les variables d'environnement depuis le fichier .env
 load_dotenv()
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key=os.environ.get("OPENAI_API_KEY")
+    api_key=os.environ.get("OPENROUTER_API_KEY")
 )
 
 completion = client.chat.completions.create(
